72bbaac09e9496a6e51e724527e82a7b197a214d
Fabio Dos Santos <fabio2st@yahoo.com>
2020-05-05 22:58:40 -0300

    Operaciones y funciones aritméticas

---
+++ VariablesOperaciones/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort contador = 1;
+            Console.WriteLine(contador);
+            contador += 1;
+            Console.WriteLine(contador);
+            contador ++;
+            Console.WriteLine(contador);
+            contador++;
+            Console.WriteLine(contador);
+            ++contador;
+            Console.WriteLine(contador);//uint acumulador = 0;
+            //Console.WriteLine(acumulador);
+            //acumulador += Convert.ToUInt32(Console.ReadLine());
+            //Console.WriteLine(acumulador);
+            //acumulador += Convert.ToUInt32(Console.ReadLine());
+            //Console.WriteLine(acumulador);
+            uint a = 8;
+            a *= a;
+            Console.WriteLine(a);
+            uint b = 11;
+            Console.WriteLine(a/b);
+            //Console.WriteLine(a\b);
+            Console.WriteLine(a % b);
+            Console.WriteLine(-1 * 2 % 3 + 4);
+        }
+    }
+}
