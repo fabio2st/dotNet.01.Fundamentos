ae9b85e78ff2b144ee9097eb6f5ae44e515edb75
Fabio Dos Santos <fabio2st@yahoo.com>
2020-07-28 21:48:11 -0300

    Fecha hora VB

--- VariablesOperaciones/OperacionesFechaHoraVb/Program.vb
+++ VariablesOperaciones/OperacionesFechaHoraVb/Program.vb
@@ -1,25 +1,48 @@
 Imports System
+Imports System.Globalization
 
 Module Program
     Sub Main(args As String())
-        'Invocación a constructor
-        'Console.WriteLine(date1);
-        Dim ahora As DateTime = Date.Now()
-        Console.WriteLine(ahora)
-        Dim elDiaFinal As DateTime
-        elDiaFinal = New DateTime(2000, 1, 1)
-        Console.WriteLine(elDiaFinal)
-        Dim elAsadoDelDiaFinal As DateTime = New DateTime(2000, 1, 1, 12, 30, 0)
-        Console.WriteLine(elAsadoDelDiaFinal)
-        Console.WriteLine("Hoy es el dia del año número: " & ahora.DayOfYear)
-        Console.WriteLine("La hora del asado es: " & elAsadoDelDiaFinal.TimeOfDay.ToString)
-        Console.WriteLine("El asado 60 días despues es: " & elAsadoDelDiaFinal.AddDays(60))
-        Console.WriteLine("Del dia final al asado pasaron {0} horas", DateDiff(DateInterval.Hour,
-                                                                               elDiaFinal, elAsadoDelDiaFinal))
-        elAsadoDelDiaFinal = DateAdd(DateInterval.Day, 60, elDiaFinal)
-        Console.WriteLine("El asado 60 días despues es: " & elAsadoDelDiaFinal)
-        Console.WriteLine("Del dia final al asado pasaron {0} horas", DateDiff(DateInterval.Hour,
-                                                                               elDiaFinal, elAsadoDelDiaFinal))
+        ' invocación de constructor
+        Dim dateOnly = New DateTime(2020, 8, 1)
+        Console.WriteLine("Solo fecha: " & dateOnly)
+        Dim dateTime = New DateTime(2020, 8, 1, 20, 24, 52)
+        Console.WriteLine("Fecha hora: " & dateTime)
+        Dim dateTimeDefault = New DateTime()
+        ' asignación de valor
+        Console.WriteLine("Fecha hora pedeterminada: " & dateTimeDefault)
+        Dim dateNow = Date.Now
+        Console.WriteLine("Fecha hora ahora: " & dateNow)
+        Dim dateUtc = Date.UtcNow
+        Console.WriteLine("Fecha hora UTC: " & dateUtc)
+        Dim dateToday = Date.Today
+        Console.WriteLine("Fecha hora hoy: " & dateToday)
+        ' Por conversión de cadena
+        Dim iso8601String = "20200801T20:30:52Z"
+        Dim dateISO8602 = Date.ParseExact(iso8601String, "yyyyMMddTHH:mm:ssZ", CultureInfo.InvariantCulture)
+        Console.WriteLine("Fecha hora parsed: " & dateISO8602)
+        ' Fecha hora literal SOLO VB
+        Dim dateLiteral As DateTime = #3/1/2008 7:00AM#
+        Console.WriteLine("Fecha hora parsed: " & dateLiteral)
 
+        ' Propiedades de instancia
+        Console.WriteLine("Fecha: " & dateISO8602.Date)
+        Console.WriteLine("Hora: " & dateISO8602.TimeOfDay.ToString)
+        Console.WriteLine("Mes: " & dateISO8602.Month)
+        ' Propiedades de estructura
+        Console.WriteLine("Ahora: " & Date.Now)
+        Console.WriteLine("Día del año: " & Date.Now.DayOfYear)
+        ' Metodos
+        Dim dias = 60
+        Console.WriteLine("{0} días despues es: {1}" & dateISO8602.AddDays(dias))
+        Console.WriteLine("{0} días antes es: {1}" & dateISO8602.AddDays(-dias))
+        Console.WriteLine("Compara {0} y {1}: {2}", dateNow, dateISO8602, dateISO8602.CompareTo(dateNow))
+
+        ' Calcular el intervalo entre dos fechas dates.
+        Dim intervalo = dateISO8602 - dateNow
+        Console.WriteLine("{0} - {1} = {2}", dateISO8602, dateNow, intervalo.ToString())
+        Console.WriteLine("Intervalo de dias: " & intervalo.Days)
+        Console.WriteLine("Intervalo de horas: " & intervalo.Hours)
+        Console.WriteLine("Intervalo total de Horas: " & intervalo.TotalHours)
     End Sub
 End Module
