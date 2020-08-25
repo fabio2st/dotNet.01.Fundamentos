58174f12a2e9f9cad094b93358fd2f4bf5f0e720
Fabio Dos Santos <fabio2st@yahoo.com>
2020-08-24 21:46:08 -0300

    Si entonces

---
+++ VariablesOperaciones/SiEntoncesCs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace SiEntoncesVb
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            short a;
+            Console.WriteLine("Ingrese un valor");
+            a = Conversions.ToShort(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("Es positivo");
+
+            if (a > 0)
+            {
+                Console.WriteLine("Es positivo");
+            }
+            else
+            {
+                Console.WriteLine("No Es positivo");
+            }
+
+            if (a > 0)
+                Console.WriteLine("Es positivo");
+            else if (a < 0)
+                Console.WriteLine("Es negativo");
+            else
+                Console.WriteLine("Es cero");
+        }
+    }
+}
