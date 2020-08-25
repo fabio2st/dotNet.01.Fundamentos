47e251b728a187b0f3695545f94b069e56d50392
Fabio Dos Santos <fabio2st@yahoo.com>
2020-08-24 23:09:32 -0300

    Enumeración

--- VariablesOperaciones/Enumeraciones/Program.cs
+++ VariablesOperaciones/Enumeraciones/Program.cs
@@ -22,9 +22,11 @@ namespace Enumeraciones
             Console.WriteLine("Figura {0} es {1}", figura.GetHashCode(), figura.ToString());
             figura = FigurasCartasEspañolas.Copa;
             Console.WriteLine("Figura {0} es {1}", figura.GetHashCode(), figura.ToString());
-            Console.WriteLine("¿Figura {0} es {1}?: {2}", figura.GetHashCode(), FigurasCartasEspañolas.Espada, figura == FigurasCartasEspañolas.Espada);
+            Console.WriteLine("¿Figura {0} es {1}?: {2}", 
+                figura.GetHashCode(), FigurasCartasEspañolas.Espada, figura == FigurasCartasEspañolas.Espada);
             figura = FigurasCartasEspañolas.Espada;
-            Console.WriteLine("¿Figura {0} es {1}?: {2}", figura.GetHashCode(), FigurasCartasEspañolas.Espada, figura == FigurasCartasEspañolas.Espada);
+            Console.WriteLine("¿Figura {0} es {1}?: {2}", 
+                figura.GetHashCode(), FigurasCartasEspañolas.Espada, figura == FigurasCartasEspañolas.Espada);
             bool existe;
             // asignar valor numérico a tipo numerado
             figura = (FigurasCartasEspañolas)18;
--- VariablesOperaciones/EnumeracionesVb/Program.vb
+++ VariablesOperaciones/EnumeracionesVb/Program.vb
@@ -16,9 +16,11 @@ Module Program
         Console.WriteLine("Figura {0} es {1}", figura.GetHashCode, figura.ToString)
         figura = FigurasCartasEspañolas.Copa
         Console.WriteLine("Figura {0} es {1}", figura.GetHashCode, figura.ToString)
-        Console.WriteLine("¿Figura {0} es {1}?: {2}", figura.GetHashCode, FigurasCartasEspañolas.Espada, figura = FigurasCartasEspañolas.Espada)
+        Console.WriteLine("¿Figura {0} es {1}?: {2}",
+                          figura.GetHashCode, FigurasCartasEspañolas.Espada, figura = FigurasCartasEspañolas.Espada)
         figura = FigurasCartasEspañolas.Espada
-        Console.WriteLine("¿Figura {0} es {1}?: {2}", figura.GetHashCode, FigurasCartasEspañolas.Espada, figura = FigurasCartasEspañolas.Espada)
+        Console.WriteLine("¿Figura {0} es {1}?: {2}",
+                          figura.GetHashCode, FigurasCartasEspañolas.Espada, figura = FigurasCartasEspañolas.Espada)
 
         Dim existe As Boolean
         ' asignar valor numérico a tipo numerado
