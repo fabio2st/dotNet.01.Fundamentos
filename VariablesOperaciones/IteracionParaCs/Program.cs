cbf86ed98119c0bcb7c725e98a66b0dff1d57a36
Fabio Dos Santos <fabio2st@yahoo.com>
2026-07-07 05:25:37 -0300

    Add loop to multiply 142857 until result exceeds 999999

--- VariablesOperaciones/IteracionParaCs/Program.cs
+++ VariablesOperaciones/IteracionParaCs/Program.cs
@@ -20,6 +20,16 @@ namespace IteracionParaCs
             // de 10 a 0 en paso -3 
             for (int x = 10; x >= 0; x -= 3)
                 Console.WriteLine("x: {0}", x);
-        }
+
+            // iterando un numero curioso 142857 hasta que el resultado sea menor a 999999
+            // la condición de parada no es necesariamente el número de iteraciones
+            int y = 0;
+            const int curiusNumber = 142857;
+            for (int x = 1; y < 999999; x++)
+            {
+                y =  curiusNumber * x;
+                Console.WriteLine("{0} x {1} = {2}", x, curiusNumber, y);
+            }
     }
+}
 }
